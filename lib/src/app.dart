9b2ca6f29e66fba75d720eabcaff23e5ae84e56d
Rangga <dwi.rangga@infoflow.co.id>
2020-02-05 14:27:41 +0700

    start use search

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -14,6 +14,7 @@ class AppState extends State<App> {
   int counter = 0;
   final url = "http://www.omdbapi.com/?apikey=c4545306";
   List<ImageModel> images = [];
+  bool typing = false;
 
   void fetchImageSearch(String str) async{
     var response = await get(url+str);
@@ -39,8 +40,116 @@ class AppState extends State<App> {
         ),*/
         appBar: AppBar(
           centerTitle: true,
-          title: Text('omdbapi.com'),
+          title: typing ? SearchBox() : Text('omdbapi.com'),
+          leading: IconButton(
+            icon: Icon(typing ? Icons.done : Icons.menu,),
+            onPressed: () {
+              setState(() {
+                typing = !typing;
+              });
+            },
+          ),
         ),
+      ),
+    );
+  }
+}
+class SearchBox extends StatelessWidget {
+  TextEditingController searchOnlyByTitleController = new TextEditingController();
+  TextEditingController searchByTitleController = new TextEditingController();
+  TextEditingController searchByYearController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: searchOnlyByTitleController,
+              decoration: InputDecoration(
+                hintText: "Search by Title...",
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              String str = searchOnlyByTitleController.text;
+              if(str.length > 0){
+                //fetchImageSearch("&s=$str");
+                searchOnlyByTitleController.text = "";
+                print("Search on front! &s=$str");
+              }              
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_to_queue,
+              color: Colors.black,
+            ),
+            onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              controller: searchByTitleController,
+                              decoration: InputDecoration(
+                                hintText: "Search by Title...",
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              controller: searchByYearController,
+                              decoration: InputDecoration(
+                                hintText: "Search by Year...",
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RaisedButton(
+                              child: Text("Submit"),
+                              onPressed: () {
+                                String strTitle = searchByTitleController.text;
+                                String strYear = searchByYearController.text;
+                                String r = "";
+                                if(strTitle.length > 0){
+                                  r += "&s=$strTitle";
+                                  searchByTitleController.text = "";
+                                }
+                                if(strYear.length > 0){
+                                  r += "&s=$strYear";
+                                  searchByYearController.text = "";
+                                }
+                                
+                                //fetchImageSearch(r);
+                                print("Search on front! $r");
+                                Navigator.pop(context);
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                });
+            },
+          ),
+        ],
       ),
     );
   }
